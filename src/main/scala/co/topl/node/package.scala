bd43bfa2cf6a94c5cf62d2c49d5691dd241f437e
James Aman <jim.a.aman@gmail.com>
2022-11-17 00:06:46 -0600

    update on while having connectivity

--- src/main/scala/co/topl/node/package.scala
+++ src/main/scala/co/topl/node/package.scala
@@ -2,7 +2,12 @@ package co.topl
 
 package object node {
   object Models {
-    trait Blob
+
+    trait Blob 
+
+    object Blob {
+      type Id = Array[Byte]
+    }
 
     type Metadata = Option[Array[Byte]]
     type Root = Array[Byte]
--- src/main/scala/co/topl/quivr/algebras/DigestVerifier.scala
+++ src/main/scala/co/topl/quivr/algebras/DigestVerifier.scala
@@ -1,5 +1,7 @@
 package co.topl.quivr.algebras
 
-import co.topl.quivr.{User, runtime}
+import co.topl.quivr.{runtime}
+
+case class Digest
 
 trait DigestVerifier[F[_]] extends ContextlessValidation[F, runtime.Error, (User.Preimage, User.Digest)]
