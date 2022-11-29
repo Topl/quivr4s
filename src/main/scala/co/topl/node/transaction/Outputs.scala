208fa15374cbf0377000917c8d99940e33d32596
James Aman <jim.a.aman@gmail.com>
2022-11-28 20:08:09 -0600

    adding common opts field to Output

--- src/main/scala/co/topl/node/transaction/Outputs.scala
+++ src/main/scala/co/topl/node/transaction/Outputs.scala
@@ -1,12 +1,13 @@
 package co.topl.node.transaction
 
-import co.topl.node.{Address, Reference}
 import co.topl.node.box.{Blob, Lock, Value}
+import co.topl.node.{Address, Reference}
 import co.topl.quivr.runtime.Datum
 
-sealed abstract class Output {
+sealed abstract class Output[O] {
   val value: Value
   val datum: Datum
+  val opts: List[Option[O]]
 }
 
 object Outputs {
@@ -16,13 +17,13 @@ object Outputs {
     attestation: Attestation,
     value:       Value,
     datum:       Datums.SpentOutput,
-    locksOpt:    List[Option[Lock]] = List()
-  ) extends Output
+    opts:        List[Option[Lock]]
+  ) extends Output[Lock]
 
   case class Unspent(
-    address:  Address,
-    value:    Value,
-    datum:    Datums.UnspentOutput,
-    blobsOpt: List[Option[Blob]] = List()
-  ) extends Output
+    address: Address,
+    value:   Value,
+    datum:   Datums.UnspentOutput,
+    opts:    List[Option[Blob]]
+  ) extends Output[Blob]
 }
