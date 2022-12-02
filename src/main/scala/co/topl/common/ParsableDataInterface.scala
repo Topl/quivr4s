f6f1096b4592b53c823c5d8a79cb9840dde75e10
James Aman <jim.a.aman@gmail.com>
2022-12-01 19:13:25 -0600

    fixing small bug in return type

--- src/main/scala/co/topl/common/ParsableDataInterface.scala
+++ src/main/scala/co/topl/common/ParsableDataInterface.scala
@@ -2,5 +2,5 @@ package co.topl.common
 
 trait ParsableDataInterface[F[_]] {
   val data: Data
-  def parse[E, T](f: Data => Either[E, T]): Either[E, T] = f(data)
+  def parse[E, T](f: Data => F[Either[E, T]]): F[Either[E, T]] = f(data)
 }
