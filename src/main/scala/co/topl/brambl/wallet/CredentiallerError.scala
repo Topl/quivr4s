da3754d5c51a34a7c246311a152c582808b57530
Diadem Shoukralla <d.shoukralla@topl.me>
2023-01-03 15:34:17 -0500

    Updated proveInput; prove will not fail if an input is not associated with a known identifier since that is a valid transaction. Instead, the proof's that require it's information will remain none. Also added error if the length of challenges and responses do not match.

--- src/main/scala/co/topl/brambl/wallet/CredentiallerError.scala
+++ src/main/scala/co/topl/brambl/wallet/CredentiallerError.scala
@@ -1,14 +1,13 @@
 package co.topl.brambl.wallet
 
-import co.topl.node.KnownIdentifier
-import co.topl.node.transaction.authorization
+import co.topl.node.transaction.{Attestation, authorization}
 
 sealed abstract class CredentiallerError
 
 object CredentiallerErrors {
 
   abstract class ProverError extends CredentiallerError
-  case class KnownIdentifierUnknown(knownIdentifier: KnownIdentifier) extends ProverError
+  case class AttestationMalformed(attestation: Attestation) extends ProverError
   case class ValidationError(error: authorization.ValidationError) extends CredentiallerError
 
 }
