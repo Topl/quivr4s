65dffe1f410702d8c0b5921fdbff2af9141ec349
Diadem Shoukralla <d.shoukralla@topl.me>
2023-01-11 16:56:48 -0500

    Updated implementation of MockTransactionBuilder

--- src/main/scala/co/topl/brambl/builders/MockTransactionBuilder.scala
+++ src/main/scala/co/topl/brambl/builders/MockTransactionBuilder.scala
@@ -3,10 +3,13 @@ package co.topl.brambl.builders
 import co.topl.brambl.models.Indices
 import co.topl.brambl.models.Datum.{IoTransaction => IoTransactionDatum, SpentOutput => SpentOutputDatum, UnspentOutput => UnspentOutputDatum}
 import co.topl.brambl.models.box.{Lock, Value}
-import co.topl.brambl.models.transaction.{IoTransaction, SpentTransactionOutput}
+import co.topl.brambl.models.transaction.{IoTransaction, SpentTransactionOutput, UnspentTransactionOutput}
 
 
 object MockTransactionBuilder extends TransactionBuilder {
+  private def collectResult[T](result: List[Either[BuilderError, T]]): (List[BuilderError], List[T]) =
+    result.partitionMap[BuilderError, T](identity)
+
   // Change needs to be explicitly added as an output.
   // Fee is whatever is left over after the sum of the outputs is subtracted from the sum of the inputs.
   override def constructUnprovenTransaction(
@@ -19,13 +22,20 @@ object MockTransactionBuilder extends TransactionBuilder {
                                     locks: List[Lock],
                                     outputValues: List[Value],
                                     datum: Option[IoTransactionDatum]
-                                  ): Either[BuilderError, IoTransaction] = {
+                                  ): Either[List[BuilderError], IoTransaction] = {
     // TODO: Ensure parallel lists are the same length
-    val builtInputs = for (i <- inputIndices.indices.toList)
-      yield MockInputBuilder.constructUnprovenInput(inputIndices(i), inputDatums(i))
-    val x: (List[BuilderError], List[SpentTransactionOutput]) = builtInputs.partitionMap[BuilderError, SpentTransactionOutput]
-    val builtOutputs = for (i <- outputIndices.indices)
-      yield MockOutputBuilder.constructOutput(outputIndices(i), outputDatums(i), locks(i), outputValues(i))
+    val inputs = collectResult[SpentTransactionOutput] {
+      for (i <- inputIndices.indices.toList)
+        yield MockInputBuilder.constructUnprovenInput(inputIndices(i), inputDatums(i))
+    }
+    val outputs = collectResult[UnspentTransactionOutput] {
+      for (i <- outputIndices.indices.toList)
+        yield MockOutputBuilder.constructOutput(outputIndices(i), outputDatums(i), locks(i), outputValues(i))
+    }
 
+    if(inputs._1.isEmpty && outputs._1.isEmpty)
+      Right(IoTransaction(inputs._2, outputs._2, datum))
+    else
+      Left(inputs._1 ++ outputs._1)
   }
 }
