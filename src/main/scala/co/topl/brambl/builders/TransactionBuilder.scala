80eb2b154b81783490f51cf953552fe7193adb1e
Diadem Shoukralla <d.shoukralla@topl.me>
2023-01-10 19:29:05 -0500

    Added list of locks to map to the outputs. This constructTransaction5 will need to be updated to take in some sort of request object

--- src/main/scala/co/topl/brambl/builders/TransactionBuilder.scala
+++ src/main/scala/co/topl/brambl/builders/TransactionBuilder.scala
@@ -2,11 +2,8 @@ package co.topl.brambl.builders
 
 import co.topl.brambl.models.{Address, Indices}
 import co.topl.brambl.models.transaction.IoTransaction
-import co.topl.brambl.models.Datum.{
-  IoTransaction => IoTransactionDatum,
-  UnspentOutput => UnspentOutputDatum,
-  SpentOutput => SpentOutputDatum
-}
+import co.topl.brambl.models.Datum.{IoTransaction => IoTransactionDatum, SpentOutput => SpentOutputDatum, UnspentOutput => UnspentOutputDatum}
+import co.topl.brambl.models.box.Lock
 
 trait TransactionBuilder {
   // Only considering single inputs for now so I don't have to think about box selection algorithm
@@ -80,8 +77,29 @@ trait TransactionBuilder {
   def constructTransaction4(
                              inputIndices: List[Indices],
                              inputDatums: List[Option[SpentOutputDatum]],
+                             outputIndices: List[Indices], // The output indices need to end up as a list of addresses
+                                                           // Addresses contain the identifier
+                                                           // The identifier will need to encode the lock
+                             outputDatums: List[Option[UnspentOutputDatum]],
+                             quantities: List[Long],
+                             datum: Option[IoTransactionDatum]
+                           ): Either[BuilderError, IoTransaction]
+
+  /**
+   * Construct simple transaction
+   *
+   * 3:a:A => 1:a:A, 1:a:B, 1:a:C
+   *
+   * Like above but considers the locks of the outputs
+   *
+   * TODO: Create some kind of request object to encompass the parallel lists
+   */
+  def constructTransaction5(
+                             inputIndices: List[Indices],
+                             inputDatums: List[Option[SpentOutputDatum]],
                              outputIndices: List[Indices],
                              outputDatums: List[Option[UnspentOutputDatum]],
+                             locks: List[Lock],
                              quantities: List[Long],
                              datum: Option[IoTransactionDatum]
                            ): Either[BuilderError, IoTransaction]
