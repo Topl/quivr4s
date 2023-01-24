2d0b3cf742ce1b2b35a8d8cb187da82a43f936e4
Diadem Shoukralla <d.shoukralla@topl.me>
2023-01-24 11:48:26 -0500

    Removed the brambl packages with no dependencies

--- src/main/scala/co/topl/brambl/builders/LockBuilder.scala
+++
@@ -1,6 +0,0 @@
-package co.topl.brambl.builders
-
-/**
- * Defines a builder for Locks
- */
-trait LockBuilder
--- src/main/scala/co/topl/brambl/transaction/validators/syntax/TransactionSyntaxVerifier.scala
+++
@@ -1,6 +0,0 @@
-package co.topl.brambl.transaction.validators.syntax
-
-import co.topl.brambl.models.transaction.IoTransaction
-import co.topl.common.ContextlessValidation
-
-trait TransactionSyntaxVerifier[F[_]] extends ContextlessValidation[F, TransactionSyntaxError, IoTransaction]
